15f32aec04fd70980c22428ae035c9e1e3fca1a1
chenxy <9985227@qq.com>
2022-10-01 21:14:00 +0800

    删除和第一个目录对比文件，默认参数删除不重复的，del_not_in如果False那么就是删除重复的 目录内重复的保留上层文件。

--- filesco.py
+++ filesco.py
@@ -1,6 +1,9 @@
 """
-只要把目录名传入main就可以删除重复
-目录都是和第一个参数进行对比，除第一个文档外有重复的全部删除
+    只要把目录名传入main
+    目录都是和第一个参数进行对比，
+    除第一个外，目录内有重复的文件名的删除重复的（保留上层的文件）。
+    参数:
+    del_not_in:默认为True,删除b不在a中如果是False，删除b在a中的。（注意：重点）
 """
 import os, time
 import pandas as pd
@@ -14,7 +17,7 @@ def get_files(filepath):
         for f in fs:
             fullf = os.path.join(p, f)
             filenames[fullf] = f
-    return pd.Series(filenames)
+    return pd.Series(filenames, dtype='object')
 
 
 def delefiles(in_files):
@@ -23,30 +26,41 @@ def delefiles(in_files):
         executor.map(os.remove, in_files)
 
 
-def main(*filedirs):
+def main(*filedirs, del_not_in):
     st = time.perf_counter()
     # 多线程读取文件名
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(get_files, filedir) for filedir in filedirs]
     r = ([x.result() for x in futures])
 
-    # 包含在第一个目录中的文档
-    in_files = pd.concat(x[x.isin(r[0])] for x in r[1:])
-    # 重复的文档
-    dupli_files = pd.concat((se := x[~x.isin(r[0])])
+    if del_not_in:
+        # 不包含在第一个目录中的文件
+        del_files = pd.concat(x[~x.isin(r[0])] for x in r[1:])
+    else:
+        # 包含在第一个目录中的文件
+        del_files = pd.concat(x[x.isin(r[0])] for x in r[1:])
+
+    # 目录内不在删除范围内的重复文件
+    dupli_files = pd.concat((se := x[~x.isin(del_files)])
                             [se.duplicated(keep='first')]
                             for x in r[1:])
-    all_deletes = pd.concat((in_files, dupli_files)).index
+
+    all_deletes = pd.concat((del_files, dupli_files)).index.drop_duplicates()
+    # print(all_deletes, not_in_files, in_files)
     # 多线程删除
     delefiles(all_deletes)
 
-    print(f'删除包含文件：{len(in_files)}个文件；\n删除重复文件：{len(dupli_files)}个。\n用时：{time.perf_counter() - st:.2f}秒')
+    print(f'删除{"不包含" if del_not_in else "包含"}文件：{len(all_deletes)}个文件；\n'
+          f'删除重复文件：{len(dupli_files)}个'
+          f'用时：{time.perf_counter() - st:.2f}秒')
 
 
 if __name__ == '__main__':
     '''
-    只要把目录名传入main就可以删除重复
+    只要把目录名传入main
     目录都是和第一个参数进行对比，
     除第一个外，目录内有重复的文件名的删除重复的（保留上层的文件）。
+    参数:
+    del_not_in:默认为True,删除b不在a中。如果是False，删除b在a中的。（注意：重点）
     '''
-    main(r'D:\temp\a', r'D:\temp\b', r'D:\temp\c')
+    main(r'D:\temp\a', r'D:\temp\b', r'D:\temp\c', del_not_in=True)
