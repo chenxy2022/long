b60df2ced995b6b27637fb79a83e11f3c9293313
chenxy2022 <cxywz@qq.com>
2022-10-10 15:18:04 +0800

    用账号密码登陆，自动获取token。

--- aio_mihuiai.py
+++ aio_mihuiai.py
@@ -1,18 +1,12 @@
-# import json
 import os
-# import re
 import time
-from multiprocessing.dummy import Process
 from time import sleep
 
 import aiofiles
 import aiohttp
 import asyncio
-# import chardet
 import pandas as pd
-# import numpy as np
 from aiohttp import ClientPayloadError
-# from lxml import etree
 from retrying import retry
 from tqdm import tqdm
 
@@ -23,7 +17,7 @@ class Spider(object):
     默认路径为当前文件下的downpic目录，此目录如果不存在会自动生成
     """
 
-    def __init__(self, down_path='', date=False):
+    def __init__(self, userdata, down_path='', date=False):
         self.headers = {'Host': 'www.mihuiai.com',
                         'Origin': 'http://www.mihuiai.com',
                         'Referer': 'http://www.mihuiai.com/mall',
@@ -32,7 +26,7 @@ class Spider(object):
         self.img_headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
         }
-        self.token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2NjYzN180XzAiLCJNSUlCVXdJQkFEQU5CZ2txaGtpRzl3MEJBUUVGQUFTQ0FUMHdnZ0U1QWdFQUFrRUEwdmZ2eVRkR0prZGJIa0I4bXAwZjNGRTBHWVAzQVlQYUpGN2pVZDFNMFh4RlNFMmNlSzNrMiI6IjljZGQwNSIsImV4cCI6MTY2NTkwNjExMSwiaWF0IjoxNjY1MzAxMzExfQ.2q4VpP7PrH4OXAIBkdYwiGOX-Re9tW9G_rJdUGqRd_PXeENC-4yOTU_e9Bwi5Xg8bkXLxVfpa60jgl-hhJNgIQ'
+        self.token = ""
         self.json = {'orderBy': "random", 'isHot': 0, 'sort': "gmtShowHome", 'start': 0, 'isTop': 'false',
                      'limit': 10000,
                      }
@@ -45,12 +39,31 @@ class Spider(object):
 
         self.down_path = down_path
         self.url = "http://www.mihuiai.com/api/resourceManage/queryPattern?_dt={}"
-        self.params = dict()
         self.limit = 35  # tcp连接数
         self.done = False
         self.date = date
         self.page = 0
-        self.headers['token'] = self.token
+        self.userdata = userdata
+
+    async def get_token(self):
+        json = {
+            "userType": 4,
+            "userTypeExt": 0
+        }
+        json.update(self.userdata)
+        url = 'http://www.mihuiai.com/api/user-login/login'
+        headers = {
+            "Host": "www.mihuiai.com",
+            "Origin": "http://www.mihuiai.com",
+            "Referer": "http://www.mihuiai.com/login",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+        }
+        async with aiohttp.ClientSession(headers=headers, ) as session:
+            async with session.post(url, json=json) as respone:
+                r = await respone.json()
+                self.token = r['data']['token']
+                self.headers['token'] = self.token
+                # print(self.token)
 
     async def _get_content(self, link, session):  # 传入的是图片连接
         try:
@@ -88,20 +101,20 @@ class Spider(object):
             self.num += 1
 
     @retry(stop_max_attempt_number=5, wait_fixed=10000)  # 如果出错10秒后重试，最多重试5次
-    async def run(self, startpage=0):
+    async def run(self, startpage=1):
         """
-        startpange:开始爬取的页面，默认为0
+        startpange:开始爬取的页面，默认为1
         """
-        start = time.time()
+        print('开始爬数据，请耐心等待！单页爬取模式，无进度条。')
         self.done = False
         self.num = 0
         self.page = 0
-
+        if not self.token: await self.get_token()  # 设置token值
         async with aiohttp.TCPConnector(limit=self.limit) as conn:  # 限制tcp连接数
             async with aiohttp.ClientSession(connector=conn, headers=self.headers) as session:
-                t = Process(target=self.jdt, )  # 进度条
-                t.daemon = True
-                t.start()
+                # t = Process(target=self.jdt, )  # 进度条
+                # t.daemon = True
+                # t.start()
 
                 start = (startpage - 1) * self.json['limit']
 
@@ -113,7 +126,7 @@ class Spider(object):
                     # if self.page>4:break # 测试用的
 
         self.done = True
-        t.join()
+        # t.join()
         print(f'一共爬取{self.page}页，{self.num}张图片')
 
     def jdt(self, ):
@@ -132,12 +145,13 @@ class Spider(object):
 
 
 def main():
-    down_path = r'E:\Download'
+    down_path = r'D:\Download'
     startpage = 1
-    spider = Spider(down_path)
-    # spider.token='' # 如果登陆过期了，这里更新
+    userdata = {"username": "18026395657",
+                "password": "1120005756", }  # 用户名密码
+    spider = Spider(userdata, down_path, )
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(spider.run(startpage, ))
+    loop.run_until_complete(spider.run(startpage))
 
 
 if __name__ == '__main__':
