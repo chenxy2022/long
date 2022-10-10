b011059cedc3856124acedf8bb311bb3ad688138
chenxy2022 <cxywz@qq.com>
2022-10-10 11:05:18 +0800

    不用pandas，改用list，速度更快。

--- deletefiles.py
+++ deletefiles.py
@@ -1,5 +1,6 @@
-import os, time
-import pandas as pd
+import os
+import time
+# import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -10,18 +11,24 @@ def delefiles(in_files):
 
 
 def get_files(filepath):
-    filenames = dict()
-    fileandpath = os.walk(filepath)
-    for p, subps, fs in fileandpath:
-        for f in fs:
-            fullf = os.path.join(p, f)
-            filenames[fullf] = f
-    return pd.Series(filenames, dtype='object')
+    # filenames = dict()
+    # fileandpath = os.walk(filepath)
+    # for p, subps, fs in fileandpath:
+    #     for f in fs:
+    #         fullf = os.path.join(p, f)
+    #         filenames[fullf] = f
+    filenames = [os.path.join(p, f)
+                 for p, subps, fs in os.walk(filepath)
+                 for f in fs]
+    # return pd.Series(filenames, dtype='object')
+    return filenames
 
 
 if __name__ == '__main__':
-    filepath = r'e:\download' # 删除这个目录和子目录下所有文件
+    filepath = r'E:\download'  # 删除这个目录和子目录下所有文件
     st = time.perf_counter()
-    se = get_files(filepath)
-    delefiles(se.index)
-    print(f'删除文件：{len(se.index)}', f'用时：{time.perf_counter() - st:.2f}秒')
+    # se = get_files(filepath)
+    # delefiles(se.index)
+    filenames = get_files(filepath)
+    delefiles(filenames)
+    print(f'删除文件：{len(filenames)}', f'用时：{time.perf_counter() - st:.2f}秒')
